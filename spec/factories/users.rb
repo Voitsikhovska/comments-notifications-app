c4e1d895fa3003fd6a1db6821a2bc95b6153ad46
Daryna Voitsikhovska <daravojka505@gmail.com>
2026-05-31 14:19:16 +0300

    [FT-2] Fixed rubocop warnings and updated CI

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :user do
-    username { Faker::Internet.unique.username(specifier: 5..15, separators: ["_"]) }
+    username { Faker::Internet.unique.username(specifier: 5..15, separators: [ "_" ]) }
     email    { Faker::Internet.unique.email }
     password { "Password1!" }
     password_confirmation { "Password1!" }
   end
 end
-
